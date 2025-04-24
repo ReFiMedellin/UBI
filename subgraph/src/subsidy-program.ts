da329ca12abcf67c7f7ef698d6da8304a09897da
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2025-04-23 19:06:39 -0500

    feat: update subgraph

--- subgraph/src/subsidy-program.ts
+++ subgraph/src/subsidy-program.ts
@@ -1,145 +1,43 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import {
   BeneficiaryAdded as BeneficiaryAddedEvent,
   BeneficiaryRemoved as BeneficiaryRemovedEvent,
-  ClaimIntervalSet as ClaimIntervalSetEvent,
-  ClaimableAmountSet as ClaimableAmountSetEvent,
-  FundsAdded as FundsAddedEvent,
-  FundsWithdrawed as FundsWithdrawedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   SubsidyClaimed as SubsidyClaimedEvent,
-  TokenAddressSet as TokenAddressSetEvent
 } from "../generated/SubsidyProgram/SubsidyProgram"
 import {
-  BeneficiaryAdded,
-  BeneficiaryRemoved,
-  ClaimIntervalSet,
-  ClaimableAmountSet,
-  FundsAdded,
-  FundsWithdrawed,
-  OwnershipTransferred,
-  SubsidyClaimed,
-  TokenAddressSet
+  Beneficiary
 } from "../generated/schema"
 
 export function handleBeneficiaryAdded(event: BeneficiaryAddedEvent): void {
-  let entity = new BeneficiaryAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let entity = new Beneficiary(
+    event.params.beneficiaryAddress
   )
-  entity.beneficiaryAddress = event.params.beneficiaryAddress
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.totalClaimed = new BigInt(0)
+  entity.dateAdded = event.block.timestamp
+  entity.dateRemoved = null
+  entity.isActive = true
 
   entity.save()
 }
 
 export function handleBeneficiaryRemoved(event: BeneficiaryRemovedEvent): void {
-  let entity = new BeneficiaryRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beneficiaryAddress = event.params.beneficiaryAddress
+  let entity = Beneficiary.load(event.params.beneficiaryAddress)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!entity) return
 
-  entity.save()
-}
-
-export function handleClaimIntervalSet(event: ClaimIntervalSetEvent): void {
-  let entity = new ClaimIntervalSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.interval = event.params.interval
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleClaimableAmountSet(event: ClaimableAmountSetEvent): void {
-  let entity = new ClaimableAmountSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleFundsAdded(event: FundsAddedEvent): void {
-  let entity = new FundsAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.amount = event.params.amount
-  entity.contractBalance = event.params.contractBalance
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleFundsWithdrawed(event: FundsWithdrawedEvent): void {
-  let entity = new FundsWithdrawed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.amountWithdrawed = event.params.amountWithdrawed
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.isActive = false
+  entity.dateRemoved = event.block.timestamp
 
   entity.save()
 }
 
 export function handleSubsidyClaimed(event: SubsidyClaimedEvent): void {
-  let entity = new SubsidyClaimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beneficiaryAddress = event.params.beneficiaryAddress
-  entity.amount = event.params.amount
-  entity.contractBalance = event.params.contractBalance
+  let entity = Beneficiary.load(event.params.beneficiaryAddress)
+  
+  if (!entity) return
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTokenAddressSet(event: TokenAddressSetEvent): void {
-  let entity = new TokenAddressSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenAddress = event.params.tokenAddress
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.totalClaimed = entity.totalClaimed.plus(event.params.amount)
 
   entity.save()
 }
