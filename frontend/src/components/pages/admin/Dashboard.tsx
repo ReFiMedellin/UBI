f70b7dae3d993d23d2da729fddb46e9ec1571fb7
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2025-05-07 00:07:10 -0500

    feat: improve UX

--- frontend/src/components/pages/admin/Dashboard.tsx
+++ frontend/src/components/pages/admin/Dashboard.tsx
@@ -3,16 +3,60 @@ import { getBuiltGraphSDK } from '@/../.graphclient';
 import { useQuery } from '@tanstack/react-query';
 import { formatUnits } from 'viem';
 import { Card } from '@/components/ui/card';
+import { useToast } from '@/hooks/use-toast';
+import { useMemo, useState } from 'react';
+import { ArrowUpDown } from 'lucide-react';
 
 const sdk = getBuiltGraphSDK();
 
 function Dashboard() {
+  type beneficiary_fields = 'id' | 'dateAdded' | 'dateRemoved' | 'isActive' | 'totalClaimed';
+  const { toast } = useToast();
+  const [sortConfig, setSortConfig] = useState<{key: beneficiary_fields; direction: 'asc' | 'desc' } | null>(null);
+
+
   const result_beneficiaries = useQuery({ queryKey: ['Beneficiaries'], queryFn: () => sdk.Beneficiaries() });
   const result_funds = useQuery({ queryKey: ['Funds'], queryFn: () => sdk.Funds() });
 
   const { data: data_beneficiaries } = result_beneficiaries;
   const { data: data_funds } = result_funds;
-  const funds = data_funds?.funds_collection[0]
+  const funds = data_funds?.funds_collection[0];
+
+  const sortedBeneficiaries = useMemo(() => {
+    if (!data_beneficiaries?.beneficiaries) return [];
+    const sorted = [...data_beneficiaries.beneficiaries];
+    if (sortConfig !== null) {
+      sorted.sort((a, b) => {
+        let aVal = a[sortConfig.key];
+        let bVal = b[sortConfig.key];
+        if (sortConfig.key === 'totalClaimed') {
+          aVal = BigInt(aVal || 0);
+          bVal = BigInt(bVal || 0);
+        }
+        if (aVal === bVal) return 0;
+        if (sortConfig.direction === 'asc') {
+          return aVal > bVal ? 1 : -1;
+        } else {
+          return aVal < bVal ? 1 : -1;
+        }
+      });
+    }
+    return sorted;
+  }, [data_beneficiaries?.beneficiaries, sortConfig]);
+
+  const requestSort = (key: beneficiary_fields) => {
+    let direction: 'asc' | 'desc' = 'desc';
+    if (sortConfig?.key === key && sortConfig.direction === 'desc') {
+      direction = 'asc';
+    }
+    setSortConfig({ key, direction });
+  }
+
+  console.log(data_beneficiaries?.beneficiaries[0])
+
+  const SortIcon = () => {
+    return <ArrowUpDown className='inline ml-1 w-4 h-4 relative top-[0.5px]'/>;
+  };
 
   return (
     <Card className='w-full p-[5px]'>
@@ -21,16 +65,27 @@ function Dashboard() {
       <TableHeader className='sticky top-0 bg-background'>
         <TableRow>
           <TableHead className='font-bold'>Dirección</TableHead>
-          <TableHead className='font-bold'>Activo</TableHead>
-          <TableHead className='font-bold'>Fecha añadido</TableHead>
-          <TableHead className='font-bold'>Fecha eliminado</TableHead>
-          <TableHead className='text-right font-bold'>Total Reclamado</TableHead>
+          <TableHead onClick={() => requestSort('isActive')} className='font-bold cursor-pointer'>
+            Activo { SortIcon() }
+          </TableHead>
+          <TableHead onClick={() => requestSort('dateAdded')} className='font-bold cursor-pointer'>
+            Fecha añadido { SortIcon() }
+          </TableHead>
+          <TableHead onClick={() => requestSort('dateRemoved')} className='font-bold cursor-pointer'>
+            Fecha eliminado { SortIcon() }
+          </TableHead>
+          <TableHead onClick={() => requestSort('totalClaimed')} className='text-right font-bold cursor-pointer'>
+            Total Reclamado { SortIcon() }
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody className='text-left'>
-        {data_beneficiaries?.beneficiaries.map((beneficiary) => (
+        {sortedBeneficiaries.map((beneficiary) => (
           <TableRow key={beneficiary.id}>
-            <TableCell className='cursor-pointer' onClick={() => navigator.clipboard.writeText(beneficiary.id)}>
+            <TableCell className='cursor-pointer' onClick={() => {
+              navigator.clipboard.writeText(beneficiary.id);
+              toast({ title: 'Dirección copiada al portapapeles'})
+            }}>
               {String(beneficiary.id).slice(0, 7)}...{String(beneficiary.id).slice(-5)}
             </TableCell>
             <TableCell>{beneficiary.isActive ? "Sí" : "No"}</TableCell>
