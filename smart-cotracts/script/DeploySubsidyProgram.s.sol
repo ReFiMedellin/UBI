2fccb59732d2b211cf0b9e99d9b5644e07771b91
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2025-07-05 13:26:09 -0500

    feat: admin can add tokens and priority queue

--- smart-cotracts/script/DeploySubsidyProgram.s.sol
+++ smart-cotracts/script/DeploySubsidyProgram.s.sol
@@ -3,24 +3,27 @@ pragma solidity ^0.8.22;
 
 import {Script, console} from "forge-std/Script.sol";
 import {SubsidyProgram} from "../src/SubsidyProgram.sol";
-import {MockERC20} from "../test/mock/MockERC20.sol";
+import {MockERC20, OtherMock} from "../test/mock/MockERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeploySubsidyProgramTestnet is Script {
     uint256 constant INITIAL_SUPPLY = 1e5;
     address tokenAddress;
+    address token2Address;
 
-    function run() public returns (SubsidyProgram, address) {
+    function run() public returns (SubsidyProgram, address, address) {
         vm.startBroadcast();
         MockERC20 mockToken = new MockERC20(INITIAL_SUPPLY);
+        OtherMock mockToken2 = new OtherMock(INITIAL_SUPPLY);
         vm.stopBroadcast();
         tokenAddress = address(mockToken);
+        token2Address = address(mockToken2);
 
         vm.startBroadcast();
         SubsidyProgram subsidyProgram = new SubsidyProgram(tokenAddress);
         vm.stopBroadcast();
 
-        return (subsidyProgram, tokenAddress);
+        return (subsidyProgram, tokenAddress, token2Address);
     }
 }
 
--- smart-cotracts/src/SubsidyProgram.sol
+++ smart-cotracts/src/SubsidyProgram.sol
@@ -14,10 +14,11 @@ contract SubsidyProgram is Ownable {
 
     uint256 public subsidyClaimInterval = 7 days;
     uint256 public subsidyClaimableAmount;
-    IERC20 public token;
 
-    event FundsAdded(uint256 amount, uint256 contractBalance);
-    event FundsWithdrawed(uint256 amountWithdrawed);
+    IERC20[] public tokens;
+
+    event FundsAdded(uint256 amount, address tokenAddress, uint256 tokenBalance);
+    event FundsWithdrawed(address tokenAddress, uint256 amountWithdrawed);
     event BeneficiaryAdded(address indexed beneficiaryAddress);
     event BeneficiaryRemoved(address indexed beneficiaryAddress);
     event SubsidyClaimed(
@@ -27,10 +28,16 @@ contract SubsidyProgram is Ownable {
     );
     event ClaimIntervalSet(uint256 interval);
     event ClaimableAmountSet(uint256 amount);
-    event TokenAddressSet(address tokenAddress);
+    event TokenAdded(address indexed tokenAddress);
+    event TokenRemoved(address indexed tokenAddress);
+    event TokenPriorityChanged(
+        address indexed tokenAddress,
+        uint256 previousPriority,
+        uint256 newPriority
+    );
 
     constructor(address _tokenAddress) Ownable(msg.sender) {
-        token = IERC20(_tokenAddress);
+        tokens.push(IERC20(_tokenAddress));
     }
 
     function setClaimInterval(uint256 _interval) external onlyOwner {
@@ -43,26 +50,65 @@ contract SubsidyProgram is Ownable {
         emit ClaimableAmountSet(_amount);
     }
 
-    function setTokenAddress(address _tokenAddress) external onlyOwner {
-        token = IERC20(_tokenAddress);
-        emit TokenAddressSet(_tokenAddress);
+    function addToken(address _tokenAddress) external onlyOwner {
+        (bool alreadyAccepted,) = isAcceptedToken(_tokenAddress);
+        require(!alreadyAccepted, "The token is already whitelisted.");
+        tokens.push(IERC20(_tokenAddress));
+        emit TokenAdded(_tokenAddress);
     }
 
-    function addFunds(uint256 _amount) external {
-        require(
-            token.transferFrom(msg.sender, address(this), _amount),
-            "Transfer failed."
-        );
-        emit FundsAdded(_amount, token.balanceOf(address(this)));
+    function removeToken(address _tokenAddress) external onlyOwner {
+        (bool whitelisted, uint256 index) = isAcceptedToken(_tokenAddress);
+        require(whitelisted, "This token isn't whitelisted.");
+        require(tokens.length > 1, "Cannot remove all tokens.");
+
+        for (uint256 i = index; i < tokens.length - 1; i++) {
+            IERC20 temp = tokens[i];
+            tokens[i] = tokens[i + 1];
+            tokens[i + 1] = temp;
+        }
+
+        tokens.pop();
+        emit TokenRemoved(_tokenAddress);
     }
 
-    function withdrawFunds() external onlyOwner {
-        uint256 contractBalance = token.balanceOf(address(this));
+    function changeTokenPriority(address _tokenAddress, uint256 priority) external onlyOwner {
+        require(priority < tokens.length, "Priority number out of bounds.");
+        (bool whitelisted, uint256 index) = isAcceptedToken(_tokenAddress);
+        require(whitelisted, "This token isn't whitelisted");
+        IERC20 tokenToChange = tokens[index];
+        tokens[index] = tokens[priority];
+        tokens[priority] = tokenToChange;
+
+        emit TokenPriorityChanged(_tokenAddress, index, priority);
+        emit TokenPriorityChanged(address(tokens[priority]), priority, index); // Maybe this isn't a good API
+    }
+
+    function addFunds(uint256 _amount, address _tokenAddress) external {
+        (bool accepted, uint256 index) = isAcceptedToken(_tokenAddress);
+
+        require(accepted, "This token isn't whitelisted.");
         require(
-            token.transfer(msg.sender, contractBalance),
+            tokens[index].transferFrom(msg.sender, address(this), _amount),
             "Transfer failed."
         );
-        emit FundsWithdrawed(contractBalance);
+
+        emit FundsAdded(
+            _amount,
+            address(tokens[index]),
+            tokens[index].balanceOf(address(this))
+        );
+    }
+
+    function withdrawFunds(address _tokenAddress) external onlyOwner {
+        (bool accepted, uint256 index) = isAcceptedToken(_tokenAddress);
+        require(accepted, "The contract doesn't have this token in balance.");
+        uint256 contractBalance = tokens[index].balanceOf(address(this));
+        require(
+            tokens[index].transfer(msg.sender, contractBalance),
+            "Transfer failed."
+        );
+        emit FundsWithdrawed(address(tokens[index]), contractBalance);
     }
 
     function addBeneficiary(address _userAddress) external onlyOwner {
@@ -91,15 +137,29 @@ contract SubsidyProgram is Ownable {
 
         addressToUser[msg.sender].lastClaimed = block.timestamp;
         addressToUser[msg.sender].totalClaimed += subsidyClaimableAmount;
+        // TODO: Use uniswap and pools to swap high priority tokens for cCop
         require(
-            token.transfer(msg.sender, subsidyClaimableAmount),
+            tokens[0].transfer(msg.sender, subsidyClaimableAmount),
             "Not enough funds."
         );
         emit SubsidyClaimed(
             msg.sender,
             subsidyClaimableAmount,
-            token.balanceOf(address(this))
+            tokens[0].balanceOf(address(this))
         );
+    }
+
+    function getWhitelistedTokens() public view returns (IERC20[] memory) {
+        return tokens;
+    }
+
+    function isAcceptedToken(
+        address _tokenAddress
+      ) public view returns (bool, uint256) {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (address(tokens[i]) == _tokenAddress) return (true, i);
+        }
+        return (false, 2**255);
     }
 
     function isBeneficiary(
