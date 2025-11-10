f0588d68c5beb18d999bfedea6f9230734165d5d
dihnometry <alejandroagudelogonzalez1402@gmail.com>
2025-11-10 13:36:16 -0500

    feat: prepare schema for subgraph grafting

--- subgraph/src/subsidy-program.ts
+++ subgraph/src/subsidy-program.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   BeneficiaryAdded as BeneficiaryAddedEvent,
   BeneficiaryRemoved as BeneficiaryRemovedEvent,
